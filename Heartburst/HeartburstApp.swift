1815dcebd92f327c7e48385e51f7eb49f151f5b7
tommy.treadway <tommy.treadway@robinhood.com>
2025-12-16 15:45:01 -0500

    Initial Commit

---
+++ Heartburst/HeartburstApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeartburstApp.swift
+//  Heartburst
+//
+//  Created by Tommy Treadway on 12/16/25.
+//
+
+import SwiftUI
+
+@main
+struct HeartburstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
